186bab3b48b41f572ae8a6f66f708b4ef993b866
learrocket <33684540+learrocket@users.noreply.github.com>
2026-05-19 10:10:23 -0500

    Update password to 1903Ola and ignore .vercel directory

--- src/components/PasswordGate.tsx
+++ src/components/PasswordGate.tsx
@@ -7,7 +7,7 @@ interface PasswordGateProps {
   onSuccess: () => void;
 }
 
-const PASSWORD = '1903test';
+const PASSWORD = '1903Ola';
 
 export const PasswordGate = ({ onSuccess }: PasswordGateProps) => {
   const [password, setPassword] = useState('');
